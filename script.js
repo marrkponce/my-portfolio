13d5b055667e1b5f596df25b2967440cb29dde13
marrkponce <markponce14@live.com>
2023-05-04 15:33:52 -0700

    Created JS greetings and stylized header

--- script.js
+++ script.js
@@ -10,3 +10,10 @@ document.querySelectorAll(".nav_link").forEach( n => n.addEventListener("click",
     hamburger.classList.remove("active");
     navMenu.classList.remove("active");
 }));
+
+//Intro
+
+const greetings = ['Hello there!', 'Welcome!', 'Bienvenido!', 'Hey!', 'Hola!', 'Greetings!', 'Nice to see you!'];
+const randomGreeting = greetings[Math.floor(Math.random() * greetings.length)];
+
+document.getElementById('greeting').innerText = randomGreeting;
